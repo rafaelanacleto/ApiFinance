9bd1e564d4e8cbec382115adb305ea8418347ca3
rafael.anacleto <rafael.anacleto@7comm.com.br>
2024-04-23 11:25:21 -0300

    Iniciando camada de DTO

---
+++ ApiFinance/DTOs/CategoriaDTO.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiFinance.DTOs
+{
+    public class CategoriaDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O Nome é requerido")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O Nome do ícone é requerido")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string? IconCSS { get; set; }
+    }
+}
