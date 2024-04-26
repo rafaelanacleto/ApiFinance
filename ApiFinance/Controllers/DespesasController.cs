6e82f2304a2e4483b059286d9add426bf6fba437
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-04-26 09:33:37 -0300

    finalizado controller Despesas e iniciado Renda controller

--- ApiFinance/Controllers/DespesasController.cs
+++ ApiFinance/Controllers/DespesasController.cs
@@ -30,5 +30,57 @@ public class DespesasController : ControllerBase
         var despesasDto = _mapper.Map<IEnumerable<DespesasDTO>>(despesas);
         return Ok(despesasDto);
     }
-    
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] DespesasDTO despesasDto)
+    {
+        if (despesasDto == null)
+            return BadRequest("Dados inválidos");
+
+        var despesa = _mapper.Map<Despesas>(despesasDto);
+
+        await _repository.AddAsync(despesa);
+        return new CreatedAtRouteResult("GetDespesa", new { id = despesasDto.Id }, despesasDto);
+    }
+
+    [HttpGet("{id:int}", Name = "GetDespesa")]
+    public async Task<ActionResult<DespesasDTO>> Get(int id)
+    {
+        var despesa = await _repository.GetByIdAsync(id);
+
+        if (despesa is null)
+            return NotFound("Despesa não encontrada");
+
+        var despeDto = _mapper.Map<Despesas>(despesa);
+
+        return Ok(despeDto);
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> Put(int id, [FromBody] DespesasDTO despesaDto)
+    {
+        if (id != despesaDto.Id)
+            return BadRequest();
+
+        if (despesaDto == null)
+            return BadRequest();
+
+        var despesa = _mapper.Map<Despesas>(despesaDto);
+
+        await _repository.UpdateAsync(despesa);
+        return Ok(despesa);
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var despesa = await _repository.GetByIdAsync(id);
+        if (despesa == null)
+        {
+            return NotFound("Categoria não encontrada");
+        }
+
+        await _repository.RemoveAsync(id);
+        return Ok(despesa);
+    }
 }
