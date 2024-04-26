ae520cd45aa86890209ac06e5cada97458adb586
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-04-26 10:17:21 -0300

    Criado get renda na controller de Renda

--- ApiFinance/Controllers/RendaController.cs
+++ ApiFinance/Controllers/RendaController.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiFinance.DTOs;
+using ApiFinance.Repositories.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ApiFinance.Controllers
@@ -10,6 +13,32 @@ namespace ApiFinance.Controllers
     [Route("api/[controller]")]
     public class RendaController : ControllerBase
     {
+        private readonly IRendaRepository _repository;
+        private readonly IMapper _mapper;
+
+        public RendaController(IRendaRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RendaDTO>>> Get()
+        {
+            var rendas = _repository.GetAllAsync();
+
+            if (rendas is null)
+                return NotFound("Renda não encontrada");
+
+            var rendaDTOs = _mapper.Map<IEnumerable<RendaDTO>>(rendas);
+            return Ok(rendaDTOs);
+        }
+
+
         
+
+
+
+
     }
 }
